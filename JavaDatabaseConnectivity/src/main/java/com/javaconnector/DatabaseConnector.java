4e855ec4a0e2593c2aa610fe7f016dd803840339
leona <leona@LEO>
2025-04-07 16:18:09 -0300

    Second Commit

---
+++ JavaDatabaseConnectivity/src/main/java/com/javaconnector/DatabaseConnector.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.javaconnector;
+
+/**
+ * 
+ */
+public class DatabaseConnector {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Conexao");
+		
+	}
+
+}
